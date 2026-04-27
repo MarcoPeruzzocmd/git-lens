ea373f39829e73938386585217ca6b29822e9dbd
mperuzzo <mperuzzo@softexpert.com>
2026-04-27 13:21:25 -0300

    chore: iniciando tabelas do banco

--- database/init.sql
+++ database/init.sql
@@ -47,6 +47,17 @@
 --
 --     created_at    → TIMESTAMP, DEFAULT CURRENT_TIMESTAMP
 --                     Data/hora em que a análise foi feita (automático)
+CREATE TABLE analyses (
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    owner VARCHAR(100) NOT NULL, 
+    repo VARCHAR(100) NOT NULL,
+    stats JSON NOT NULL,
+    total_commits INT NOT NULL,
+    branch VARCHAR(100) DEFAULT 'main',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+CREATE INDEX idx_analyses_owner_repo ON analyses(owner, repo);
+
 --
 --   CRIAR UM ÍNDICE:
 --     CREATE INDEX idx_analyses_owner_repo ON analyses(owner, repo)
